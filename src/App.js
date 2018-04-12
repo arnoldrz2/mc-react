b4e954f7bcf37db5ca695f3ddf30ed026f001a85
arnoldrz2 <arnoldrz2@gmail.com>
2018-04-12 14:02:20 -0500

    basic links setup

--- src/App.js
+++ src/App.js
@@ -5,88 +5,31 @@ import ArtistSignUp from './components/ArtistSignUp';
 import HomePage from './components/HomePage';
 
 
-const Home = () => (
+const Splash = () => (
   <div>
     <SplashPage/>
   </div>
 )
 
-const About = () => (
-  <div>
-    <ArtistSignUp/>
-  </div>
-)
-
-const Topic = () => (
+const Home = () => (
   <div>
     <HomePage/>
   </div>
 )
 
-const Topics = ({ match }) => (
+const ArtSignUp = () => (
   <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic}/>
-    <Route exact path={match.path} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
+    <ArtistSignUp/>
   </div>
 )
 
-const BasicExample = () => (
+const AppLinks = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
-
-      <hr/>
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topic}/>
+      <Route exact path="/" component={Splash}/>
+      <Route path="/home" component={Home}/>
+      <Route path="/artistsignup" component={ArtSignUp}/>
     </div>
   </Router>
 )
-export default BasicExample
-
-
-
-
-
-
-// export default App;
-
-
-// Extra Code Below
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         {/* {this.props.children} */}
-//         <SplashPage/>
-//       </div>
-//     );
-//   }
-// }
+export default AppLinks;
