c62754f0c496e81fa934120b8b6ffddf16effe4b
arnoldrz2 <arnoldrz2@gmail.com>
2018-04-17 17:48:55 -0500

    added setup for usersignup POSTs to mysql db

--- mc-backend/controllers/mcrouter.js
+++ mc-backend/controllers/mcrouter.js
@@ -4,5 +4,23 @@ var router = express.Router();
 // Import the model (user.js) to use its database functions.
 var user = require("../models/user.js");
 
+router.post("/users", function(req, res) {
+    user.create([
+        "user_first_name",
+        "user_last_name",
+        "user_email",
+        "user_password",
+        "user_zip"
+    ],[
+        req.body.user_first_name,
+        req.body.user_last_name,
+        req.body.user_email,
+        req.body.user_password,
+        req.body.user_zip,
+    ], function(data) {
+        res.render("account");
+    });
+});
+
 
 module.exports = router;
--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -18,86 +18,78 @@ const UserPrefForm = () => (
         
             <div className="categoriesform-container">
                 <div className="form-container">
-                    <form action="">
+                    <form action="/users" method="POST">
                         <h2 className="CategoriesCont">Account Setup</h2>
                         {/* <!-- Name --> */}
                         <div className="row">
                             {/* <!-- First Name --> */}
                             <div className="form-group col">
                                 <label htmlFor="first_name">First Name</label>
-                                <input type="text" className="form-control" id="first_name"/>
+                                <input type="text" className="form-control" id="user_first_name"/>
                             </div>
                             {/* <!-- Last Name --> */}
                             <div className="form-group col">
                                 <label htmlFor="last_name">Last Name</label>
-                                <input type="text" className="form-control" id="last_name"/>
+                                <input type="text" className="form-control" id="user_last_name"/>
                             </div>
                         </div>
                         {/* <!-- Email --> */}
                         <div className="row">
                             <div className="form-group col">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" id="email"/>
+                                <input type="email" className="form-control" id="user_email"/>
                             </div>
                         </div>
                         {/* <!-- Password --> */}
                         <div className="row">
                             <div className="form-group col">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" id="password"/>
+                                <input type="password" className="form-control" id="user_password"/>
                             </div>
                         </div>
                         {/* Zip Code */}
                         <div className="row">
                             <div className="form-group col">
                                 <label htmlFor="zip">Zip Code</label>
-                                <input type="text" className="form-control" id="zip"/>
+                                <input type="text" className="form-control" id="user_zip"/>
                             </div>
                         </div>
                         <h2 className="CategoriesCont">Your tattoo artist preferences</h2>
                         
                         <div className="GenderSelection">
                         {/* User Preference for Artist Gender */}
-                        <p>Do you have a tattoo artist gender preference?</p>
+                        <p>Do you have a tattoo artist gender preference?(leave blank if no preference)</p>
                             <div className="row">
                                 <div className="col">
                                     <div className="form-check">
-                                        <input className="form-check-input" name="options" type="radio" id="male" value="male" />
+                                        <input className="form-check-input" name="options" type="radio" id="male" value="m" />
                                         <label className="form-check-label" htmlFor="gendermale">Male</label>
                                     </div>
                                     <div className="form-check">
-                                        <input className="form-check-input" name="options" type="radio" id="female" value="female" />
+                                        <input className="form-check-input" name="options" type="radio" id="female" value="f" />
                                         <label className="form-check-label" htmlFor="genderfemale">Female</label>
-                                    </div>
-                                    <div className="form-check">
-                                        <input className="form-check-input" name="options" type="radio" id="nopreference" value="nogenpreference" />
-                                        <label className="form-check-label" htmlFor="nogenpreference">No Preference</label>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="ArtistExperience">
                         {/* User Preference for Artist Certification */}
-                        <p> Do you have a preference for experience?</p>
+                        <p> Do you have a preference for experience?(leave blank if no preference)</p>
                             <div className="row">
                                 <div className="col">
                                     <div className="form-check">
-                                        <input className="form-check-input" name="options" type="radio" id="certified" value="certified" />
+                                        <input className="form-check-input" name="options" type="radio" id="certified" value="0" />
                                         <label className="form-check-label" htmlFor="certified">Certified</label>
                                     </div>
                                     <div className="form-check">
-                                        <input className="form-check-input" name="options" type="radio" id="notcertified" value="notcertified" />
+                                        <input className="form-check-input" name="options" type="radio" id="notcertified" value="1" />
                                         <label className="form-check-label" htmlFor="notcertified">Not Certified</label>
-                                    </div>
-                                    <div className="form-check">
-                                        <input className="form-check-input" name="options" type="radio" id="nocertpreference" value="nocertpreference" />
-                                        <label className="form-check-label" htmlFor="nocertpreference">No Preference</label>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="nextbutton">
-                            <button type="button" className="btn btn-secondary">Next</button>
+                            <button type="submit" className="btn btn-secondary">Next</button>
                         </div>
                     </form>
                 </div>
