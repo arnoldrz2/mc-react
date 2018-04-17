ee9baefd8aa57e0896f990f7d501792328d556a0
arnoldrz2 <arnoldrz2@gmail.com>
2018-04-17 16:35:59 -0500

    modified backend

---
+++ mc-backend/controllers/mcrouter.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+
+// Import the model (user.js) to use its database functions.
+var user = require("../models/user.js");
+
+
+module.exports = router;
